ba0a8358108adb53c3f3a51bc6bb7dc9009ccdf9
mansi-okay <mansiagrawat2@gmail.com>
2026-03-11 22:22:48 +0530

    user login functionality added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,6 +5,11 @@ import { User } from "../models/user.model.js"
 import {genAccessAndRefreshTokens} from "../utils/generateTokens.js"
 import { registrationMail } from "../services/email.service.js"
 
+
+/*
+- user register controller
+- POST /api/auth/register
+*/
 const userRegisterController = asyncHandler(async(req,res) => {
     const {name,email,password} = req.body
 
@@ -60,4 +65,48 @@ const userRegisterController = asyncHandler(async(req,res) => {
 )
 })
 
-export {userRegisterController}
+/*
+- user login controller
+- POST /api/auth/log-in
+*/
+const userLoginController = asyncHandler(async (req,res) => {
+    const {email,password} = req.body
+
+    if (!email?.trim() || !password?.trim()) {
+        throw new ApiError(400,"All fields required!")
+    }
+
+    const user = await User.findOne({email:email.toLowerCase()}).select("+password")
+
+    if (!user) {throw new ApiError(400, "Email or password is INVALID!")}
+
+    const passwordMatch = await user.isPasswordCorrect(password)
+
+    if (!passwordMatch) {throw new ApiError(400,"Email or password is INVALID!")}
+
+    const {accessToken,refreshToken} = await genAccessAndRefreshTokens(user._id)
+
+    const options = {
+        httpOnly:true,
+        secure:true,
+        sameSite:"strict",
+        maxAge:7*24*60*60*1000
+    }
+
+    return res.status(200)
+    .cookie("accessToken",accessToken,options)
+    .cookie("refreshToken",refreshToken,options)
+    .json(new ApiResponse(200,
+        {
+            user : {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role
+            }            
+        },
+        "User logged-in successfully"
+    ))
+})
+
+export {userRegisterController,userLoginController}
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import {userRegisterController} from "../controllers/auth.controller.js"
+import {userRegisterController,userLoginController} from "../controllers/auth.controller.js"
 
 const authRouter = Router()
 
+/* POST /api/auth/register */
 authRouter.route("/register").post(userRegisterController)
+
+/* POST /api/auth/log-in */
+authRouter.route("/log-in").post(userLoginController)
+
 
 export {authRouter}
